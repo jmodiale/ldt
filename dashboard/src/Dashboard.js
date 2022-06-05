800328f84b97225f0adc1809638c5465082faa7f
remen <remen.omodiale@outlook.com>
2022-06-05 18:42:50 +0100

    filter search

--- dashboard/src/Dashboard.js
+++ dashboard/src/Dashboard.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import { useState } from 'react';
+import { items } from './data'
 
 export default function Dashboard() {
     const [entry, setEntry] = useState("");
     const [organiser, setOrganiser] = useState("");
+    const [data, setData] = useState("items");
 
-    const handleSubmit = () =>{
+    const filteredItems = data.filter((item) => {
+      return (
+        item.firstName.toLowerCase().includes(entry.toLowerCase()) ||
+        item.lastName.toLowerCase().includes(entry.toLowerCase()) ||
+        item.eventTitle.toLowerCase().includes(entry.toLowerCase()) ||
+        item.raceTitle.toLowerCase().includes(entry.toLowerCase())
+      );
+    });
 
+    const handleSubmit = (e) =>{
+         setData(filteredItems);
+         e.preventDefault();
     }
 
     const handleClick = () => {

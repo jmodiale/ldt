0b9818f9d6edcc26856c23f467f3dd702c385417
remen <remen.omodiale@outlook.com>
2022-06-05 18:35:15 +0100

    set inputs

--- dashboard/src/Dashboard.js
+++ dashboard/src/Dashboard.js
@@ -1,7 +1,37 @@
 import React from 'react'
+import { useState } from 'react';
 
 export default function Dashboard() {
+    const [entry, setEntry] = useState("");
+    const [organiser, setOrganiser] = useState("");
+
+    const handleSubmit = () =>{
+
+    }
+
+    const handleClick = () => {
+
+    }
+
+
   return (
-    <div>Dashboard</div>
-  )
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          value={entry}
+          onChange={(e) => setEntry(e.target.value)}
+          placeholder="Search by name, event or race"
+        />
+      </form>
+      <form>
+        <input
+          type="search"
+          value={organiser}
+          onChange={(e) => setOrganiser(e.target.value)}
+          placeholder="Enter organiser Id"
+        />
+      </form>
+    </>
+  );
 }
